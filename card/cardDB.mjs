954ef26b8d20a9b510c285ba3fd08162baca8c9d
swatiitobuz <swati@itobuz.com>
2023-03-29 18:28:32 +0530

    29.03.2023

--- card/cardDB.mjs
+++ card/cardDB.mjs
@@ -1,5 +1,6 @@
 export const cardData = [
   {
+    name: "card1",
     image: "abc",
     text: "WELLNESS",
     header: "Dedicated to wellness",
@@ -7,18 +8,21 @@ export const cardData = [
       "Aman's approach to wellness is focused on personalisation, privacy and seclusion. Local spa rituals with indigenous ingredients, offer a profoundly therapeutic touch, and a gentle sense of discovery.",
   },
   {
+    name:"card2",
     image: "abc",
     text: "GIFT CARDS",
     header: "The gift of future travel",
     paragraph: "Share extraordinary experiences with the Aman E- Gift Card - delivered instantly and offering memories that last a lifetime."
   },
   {
+    name:"card3",
     image: "abc",
     text: "MIKALTO SHOP",
     header: "The spa experience from home",
     paragraph: "Skincare and Spa supplements harness powerful natural ingredients to provide balance and optimal wellbeing."
   },
   {
+    name:"card4",
     image: "",
     text: "EXPERIENCE",
     header: "Adventures in the wild",
