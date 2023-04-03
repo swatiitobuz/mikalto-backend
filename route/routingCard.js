772b78859240cc9061724a142fc88b0ca2546b45
swatiitobuz <swati@itobuz.com>
2023-04-03 17:00:45 +0530

    commited 03.04.2023

--- route/routingCard.js
+++ route/routingCard.js
@@ -1,24 +1,35 @@
 const http = require("http");
 const fs = require("fs");
+const parse = require("querystring");
 
 const server = http.createServer((req, res) => {
   console.log("a request is made on " + req.url);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.writeHead(200, { "Content-Type": "text/txt" });
+  let formDetail = "";
+  res.on("data", (formData) => {
+    formDetail = formData.toString();
+  });
+  res.on("end", () => {
+    console.log(parse(formDetail));
+  });
+  // res.end(
+  //   "Hello"
+  // )
   if (req.url === "/home" || req.url === "/") {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.writeHead(200, { "Content-Type": "text/txt" });
+    
     fs.createReadStream(
       "/Users/it012306/Desktop/node/mikalto/modules/card/cardDatabase.txt"
     ).pipe(res);
-  }
-  else{
+  } else {
     res.end("no routes");
   }
 });
-server.listen(3000, "127.0.0.1",(err)=>{
-    if(err){
-        console.log(err);
-    }
-    else{
-        console.log("success");
-    }
+server.listen(3000, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("success");
+  }
 });
---
+++ route/routingForm.mjs
@@ -0,0 +1,47 @@
+import http from "http";
+import { parse } from "querystring";
+import * as fs from "fs/promises";
+let port = "5000";
+
+async function writeDetails(data) {
+  // const accessData = JSON.parse(data);
+  let reasultofRead = await fs.readFile("./formDB.txt", "utf-8", (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      return;
+    }
+  });
+  reasultofRead += data;
+  console.log(reasultofRead);
+  await fs.writeFile("./formDB.txt", reasultofRead, (err) => {
+    if (err) {
+      console.log(TypeError);
+    }
+  });
+}
+
+const server = http.createServer((req, res) => {
+  try {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    let formData = "";
+    req.on("data", (formDataPeices) => {
+      formData = formDataPeices.toString();
+    });
+    req.on("end", () => {
+      if (Object.keys(formData).length !== 0) {
+        console.log(formData);
+        writeDetails(formData);
+      }
+    });
+    res.end("hello");
+  } catch (err) {
+    console.log(err);
+  }
+});
+server.listen(port, "127.0.0.1", (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
